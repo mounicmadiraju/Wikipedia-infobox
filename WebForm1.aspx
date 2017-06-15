af0c8f30849aa0b06197923573245baca49e07e9
Mounic Madiraju <mounicraju@gmail.com>
2017-06-15 15:00:37 +0800

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="wikicontent.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            width: 726px;
+            height: 136px;
+            }
+   
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <p>
+            <textarea id="TextArea1" runat="server" name="S1"></textarea></p>
+        <p>
+            &nbsp;</p>
+        <p>
+&nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="margin-left: 202px" Text="Export" Width="180px" />
+        </p>
+        <table width="100%">
+            <thead>
+                <tr>
+                    <th width="20%">Title</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody id="result" runat="server">
+                
+            </tbody>
+        </table>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wikicontent
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Pageval
+        {
+            public int pageid { get; set; }
+            public int ns { get; set; }
+            public string title { get; set; }
+            //public string extract { get; set; }
+            public dynamic revisions { get; set; }
+        }
+
+        public class Query
+        {
+            public Dictionary<string, Pageval> pages { get; set; }
+        }
+
+        public class Limits
+        {
+            public int extracts { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string batchcomplete { get; set; }
+            public Query query { get; set; }
+            //public Limits limits { get; set; }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string[] companies = TextArea1.InnerText.Split(',');
+            StringBuilder sb = new StringBuilder();
+            foreach (string company in companies)
+            {
+                string url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + company.Trim();
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                var responseJson = JsonConvert.DeserializeObject<RootObject>(response.Content.ReadAsStringAsync().Result);
+                var firstKey = responseJson.query.pages.First().Key;
+                var title = responseJson.query.pages[firstKey].title;
+                Object revision = responseJson.query.pages[firstKey].revisions[0];
+                var extract = revision.GetType().GetProperty("*").GetValue(revision, null);
+                
+
+                sb.Append("<tr><td>" + title + "</td><td>" + extract + "</td></tr>");
+            }
+            result.InnerHtml = sb.ToString();
+        }
+    }
+}
